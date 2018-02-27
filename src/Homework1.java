ba1cc73ad4c2a601e314a406884783adaef8eb02
Emily Richmond <emily.richmond524@gmail.com>
2018-02-27 17:38:03 -0500

    break

--- src/Homework1.java
+++ src/Homework1.java
@@ -9,7 +9,7 @@ public class Homework1 {
 
 	public static int findLast(int[] x, int y) {
 		for (int i = x.length - 1; i > 0; i--) {
-			if (x[i] == y) {
+			if (x[i] = y) {
 				return i;
 			}
 		}
